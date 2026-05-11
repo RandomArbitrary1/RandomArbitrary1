301ecc28997f35641ede4f7d5e4e302bed6fee36
Danilo <eggwardchickfila@gmail.com>
2026-05-11 13:50:24 +0200

    changed player, added sfx, changed environment and added dodge

--- assets/entities/main_character/player.gd
+++ assets/entities/main_character/player.gd
@@ -2,12 +2,19 @@ extends CharacterBody3D
 @export var min_pitch = -80.0  # Look down limit
 @export var max_pitch = 80.0 
 
-const SPEED = 7.0
-const JUMP_VELOCITY = 4.5
+const SPEED = 14
+const JUMP_VELOCITY = 7.5
+const ACCEL = 90
 var SENSITIVITY = 0.003
 var camera_rotation = Vector2.ZERO
+var HEALTH = 100
+var Invincibility = 0
 @onready var camera_pivot = $CameraPivot
 @onready var camera_arm = $CameraPivot/CameraArm
+@onready var hud_hp = $"../CanvasLayer/Hud/HP"
+@onready var dodge_sfx = $sounds/dodge
+
+
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	
@@ -18,10 +25,21 @@ func _unhandled_input(event):
 		camera_rotation.x = clamp(camera_rotation.x,
 		deg_to_rad(min_pitch),
 		deg_to_rad(max_pitch))
+	if Input.is_action_just_pressed("dodge"):
+		Invincibility = 1
+		var input_dir := Input.get_vector("left", "right", "up", "down")
+		var direction := Vector3(input_dir.x, 0, input_dir.y).rotated(Vector3.UP, camera_pivot.rotation.y).normalized()
+		var horizontal_velocity = direction * SPEED * 4
+		velocity.x = horizontal_velocity.x
+		velocity.z = horizontal_velocity.z
+		dodge_sfx.play()
 		
 func _process(_delta):
 	camera_pivot.rotation.x = camera_rotation.x  # Pitch
 	camera_pivot.rotation.y = camera_rotation.y  # Yaw
+	hud_hp.text = str(Invincibility)
+	if Invincibility > 0.0:
+		Invincibility -= _delta
 	
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -29,19 +47,18 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		#velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("left", "right", "up", "down")
 	var cam_y_rotation = camera_pivot.rotation.y
 	var direction := Vector3(input_dir.x, 0, input_dir.y).rotated(Vector3.UP, cam_y_rotation).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+	var target_velocity = direction * SPEED
+	var horizontal_velocity = velocity
+	horizontal_velocity = horizontal_velocity.move_toward(target_velocity, ACCEL * delta)
+	velocity.x = horizontal_velocity.x
+	velocity.z = horizontal_velocity.z
 
 	move_and_slide()
