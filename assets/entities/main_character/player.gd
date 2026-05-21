c1646f54cfad5458040ad90e587d761fe8898602
Danilo <eggwardchickfila@gmail.com>
2026-05-21 09:18:52 +0200

    changed player code, changed v-sync mode

--- assets/entities/main_character/player.gd
+++ assets/entities/main_character/player.gd
@@ -10,6 +10,8 @@ var camera_rotation = Vector2.ZERO
 var HEALTH = 100
 var Invincibility = 0
 var Attack_hurt_time = 0
+var char_rotation = 0.0
+var rotation_speed = 10.0
 @onready var body = $BODY
 @onready var camera_pivot = $CameraPivot
 @onready var camera_arm = $CameraPivot/CameraArm
@@ -48,8 +50,11 @@ func _physics_process(delta: float) -> void:
 	var input_dir := Input.get_vector("left", "right", "up", "down")
 	var cam_y_rotation = camera_pivot.rotation.y
 	var direction := Vector3(input_dir.x, 0, input_dir.y).rotated(Vector3.UP, cam_y_rotation).normalized()
-	body.rotation = direction
 	var target_velocity = direction * SPEED
+	if direction.length() > 0.01:
+		var target_rotation = atan2(direction.x, direction.z)
+		char_rotation = lerp_angle(char_rotation, target_rotation, rotation_speed * delta)
+		body.rotation.y = char_rotation
 	var horizontal_velocity = velocity
 	horizontal_velocity = horizontal_velocity.move_toward(target_velocity, ACCEL * delta)
 	velocity.x = horizontal_velocity.x
